062e5b60204438c76835a8963551c36b0cc1655e
Maarten Staa <maartens@spotify.com>
2024-12-03 09:50:50 +0100

    day 3, part 2

--- crates/day03/src/main.rs
+++ crates/day03/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
@@ -18,15 +19,49 @@ fn part1(input: &str) -> usize {
     sum
 }
 
+fn part2(input: &str) -> usize {
+    let regex = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut sum = 0;
+    let mut enabled = true;
+
+    for cap in regex.captures_iter(input) {
+        if cap[0].starts_with("don't") {
+            enabled = false;
+            continue;
+        } else if cap[0].starts_with("do") {
+            enabled = true;
+            continue;
+        }
+
+        if !enabled {
+            continue;
+        }
+
+        let x: usize = cap[1].parse().unwrap();
+        let y: usize = cap[2].parse().unwrap();
+
+        sum += x * y;
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str =
+    const TEST_INPUT1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const TEST_INPUT2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
-    fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 161);
+    fn test_day3_part1() {
+        assert_eq!(part1(TEST_INPUT1), 161);
+    }
+
+    #[test]
+    fn test_day3_part2() {
+        assert_eq!(part2(TEST_INPUT2), 48);
     }
 }
